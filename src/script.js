a43ec4d1afb7175eac4c0c861f9590c251be8de0
Octavian Sulic <octavius1992@gmail.com>
2023-10-09 17:12:52 +0300

    Import door textures.

--- src/script.js
+++ src/script.js
@@ -29,6 +29,14 @@ scene.add(axesHelper)
  */
 const textureLoader = new THREE.TextureLoader()
 
+const doorColorTexture = textureLoader.load('./textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('./textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('./textures/door/normal.jpg')
+const doorMetalnassTexture = textureLoader.load('./textures/door/metalnass.jpg')
+const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
+
 /**
  * House
  */
